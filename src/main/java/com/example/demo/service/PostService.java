782f84a5a47920e36648f3c8cba7ef090d362bc3
nitika1234567 <nitika.cse18@chitkarauniversity.edu.in>
2021-02-22 15:44:18 +0530

    adding and viewing Posts from db

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,14 +3,14 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.Date;
 
 @Service
 public class PostService {
     //Singleton
     private static ArrayList<Post> POSTS=new ArrayList<>();
-    static
+  /*  static
     {
         Post post1=new Post();
         post1.setTitle("World Wide");
@@ -41,13 +41,67 @@ public class PostService {
         post4.setDate(new Date());
         POSTS.add(post4);
 
-    }
+    }*/
+  private  final String url="jdbc:postgresql://localhost:5432/technicalblog";
+  private final String username="postgres";
+  private final String password="password";
+  public Connection connect() throws SQLException
+  {
+    return DriverManager.getConnection(url,username,password);
+  }
     public ArrayList<Post> getAllPosts()
-    {
+    {//BUSSINESS LOGIC FOR CONNECTING THE DATABASE
+      try {
+        //1. connect to databse
+        Connection connection=connect();
+        // 2. get/create the statement
+
+        Statement statement=connection.createStatement();
+
+        // 3. Execute the select query
+        ResultSet resultSet=statement.executeQuery("SELECT * FROM posts");
+
+        // 4.  loop into the resultset and get the data
+        while (resultSet.next())
+        {
+          Post post1 =new Post();
+          post1.setTitle(resultSet.getString("title"));
+          post1.setBody(resultSet.getString("body"));
+          post1.setDate(resultSet.getDate("date"));
+          //store data in singleton
+          POSTS.add(post1);
+        }
+
+      }
+      catch(SQLException e)
+      {
+       System.out.println(e.getMessage());
+      }
       return POSTS;
     }
     public void createPost(Post newPost)
     {
-      POSTS.add(newPost);
+      String query="INSERT INTO posts(title,body,date) VALUES(?,?,?)";
+      try
+      {
+        Connection connection=connect();
+        //2. prepare a statement
+        PreparedStatement preparedStatement=connection.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+        // fix the values from the VIEWS
+        preparedStatement.setString(1,newPost.getTitle());
+        preparedStatement.setString(2, newPost.getBody());
+        preparedStatement.setDate(3,new Date(newPost.getDate().getTime()));
+        int updatedRows = preparedStatement.executeUpdate();
+        if(updatedRows>0)
+        {
+          System.out.println("Update is working fine");
+        }
+
+
+      }
+      catch(SQLException e)
+      {
+        System.out.println(e.getMessage());
+      }
     }
 }
